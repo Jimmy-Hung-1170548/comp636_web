eb3057d807f939962aaf3676f1898f7ddd710d07
Jimmy Hung <p16981047@gs.ncku.edu.tw>
2025-09-18 16:28:14 +0800

    initialize environment'

---
+++ connect.py
@@ -0,0 +1,7 @@
+# These details are available on the first MySQL Workbench screen
+# Usually called 'Local Instance'
+dbuser = "JimmyH" #PUT YOUR MySQL username here - usually root
+dbpass = "@Vice623439" #PUT YOUR PASSWORD HERE
+dbhost = "172.24.214.37" 
+dbport = 3306
+dbname = "lincoln_db"
